323c418770a4e9d0ac870a3c3481ba60d9f4e3e5
guchvlado <guchvlado324@gmail.com>
2022-03-31 12:51:37 +0300

    added tabs(done)

--- js/script.js
+++ js/script.js
@@ -3,22 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
           addButton = document.querySelector('.button'),
           taskList = document.querySelector('.task-list');
 
-    let dataArray = [];
+    let dataArray = {
+        do: [],
+        done: []
+    };
+    let activeTab = "do"; // do / done
+
     if (localStorage.getItem("data")) {
         dataArray = JSON.parse(localStorage.getItem("data"));
     }
 
-    updateList();
+    updateList(activeTab);
 
-    function updateList() {
+    function updateList(currentTab) {
         taskList.innerHTML = "";
-        for (let i = 0; i < dataArray.length; i++) {
+        for (let i = 0; i < dataArray[currentTab].length; i++) {
             const newTask = document.createElement('div');
             newTask.classList.add('task');
             newTask.innerHTML = `
                 <span class="task-number">${i+1}</span>
-                <p class="task-text">${dataArray[i]}</p>
-                <div class="close-button">&#10006</div>
+                <p class="task-text">${dataArray[currentTab][i]}</p>
+                <div class="change-buttons">
+                    <div class="done-button">&#10003</div>
+                    <div class="close-button">&#10006</div>
+                </div>
             `;
             taskList.append(newTask);
         }
@@ -30,17 +38,24 @@ document.addEventListener("DOMContentLoaded", () => {
         const text = input.value;
         if (text == '') return 0;
 
-        dataArray.push(text);
-        updateList();
+        dataArray[activeTab].push(text);
+        updateList(activeTab);
         input.value = "";
     });
 
     taskList.addEventListener('click', (e) => {
         const target = e.target;
+        console.log(target);
         if (target && target.classList.contains('close-button')) {
-            let index = target.parentNode.querySelector('.task-number').textContent - 1;
-            dataArray = [].concat(dataArray.slice(0, index), dataArray.slice(index+1));
-            updateList();
+            let index = target.parentNode.parentNode.querySelector('.task-number').textContent - 1;
+            dataArray[activeTab] = [].concat(dataArray[activeTab].slice(0, index), dataArray[activeTab].slice(index+1));
+            updateList(activeTab);
+        }
+        else if (target && target.classList.contains('done-button') && activeTab != 'done') {
+            let index = target.parentNode.parentNode.querySelector('.task-number').textContent - 1;
+            dataArray.done.push(dataArray[activeTab][index]);
+            dataArray[activeTab] = [].concat(dataArray[activeTab].slice(0, index), dataArray[activeTab].slice(index+1));
+            updateList(activeTab);
         }
     });
 
@@ -51,6 +66,8 @@ document.addEventListener("DOMContentLoaded", () => {
         if (e.target && e.target.classList.contains("tab")) {
             tabs.forEach(item => item.classList.remove('active-tab'));
             e.target.classList.add('active-tab');
+            activeTab = e.target.getAttribute('id');
+            updateList(activeTab);
         }
     });
 });
